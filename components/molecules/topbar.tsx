ca3348959b377afd582c4ea8b959be79883509cb
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 14:30:03 -0300

    style: updating topbar molecule

--- components/molecules/topbar.tsx
+++ components/molecules/topbar.tsx
@@ -7,12 +7,12 @@ import Logo from '@/components/atoms/logo'
 
 export default function Topbar() {
   return (
-    <Container>
-      <div className='my-4 flex items-center justify-between'>
+    <div className='min-h-20 flex items-center justify-between bg-white py-4'>
+      <Container>
         <Link className='transition-all duration-200 hover:opacity-80' href='/'>
           <Logo />
         </Link>
-      </div>
-    </Container>
+      </Container>
+    </div>
   )
 }
