3a3613f57b094d1a2dd858d2147bac32a660b72b
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 12:10:18 -0300

    feat: updating constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,38 @@
-export const constants = [
+export type catalogItem = {
+  name: string
+  url: string
+}
+
+export const catalogItems: catalogItem[] = [
   {
-    name: 'Catálogo',
-    url: '/catalogo'
+    name: 'Bancadas',
+    url: '/catalogo/bancadas'
+  },
+  {
+    name: 'Bancadas Aspiradas',
+    url: '/catalogo/bancadas-aspiradas'
+  },
+  {
+    name: 'Bancadas Especiais',
+    url: '/catalogo/bancadas-especiais'
   },
   {
     name: 'Politrizes',
     url: '/catalogo/politrizes'
+  },
+  {
+    name: 'Equipamentos Diversos',
+    url: '/catalogo/equipamentos-diversos'
+  },
+  {
+    name: 'Prensas Hidráulicas',
+    url: 'catalogo/prensas-hidraulicas'
   }
 ]
+
+export function compareFn(a: catalogItem, b: catalogItem) {
+  const nameA = a.name.toUpperCase()
+  const nameB = b.name.toUpperCase()
+
+  return nameA < nameB ? -1 : nameA > nameB ? 1 : 0
+}
