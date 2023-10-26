b1bb712db0b77fc2c9410b8bb582c1550d4c0908
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 14:29:32 -0300

    feat: refactoring navbar on an organism

---
+++ components/molecules/bottom-bar.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Link from 'next/link'
+import { catalogItems, compareFn, type catalogItem } from '@/utils/constants'
+
+export default function BottomBar() {
+  return (
+    <div className='flex h-16 flex-nowrap items-center bg-primary px-6 py-4 text-primary'>
+      <ul className='flex w-full flex-nowrap items-center justify-between overflow-x-auto'>
+        {catalogItems
+          .sort(compareFn)
+          .map(({ name, url }: catalogItem, index: number) => (
+            <li
+              className='mr-4 font-medium transition-all duration-200 hover:opacity-80 md:mr-0'
+              key={index}
+            >
+              <Link className='truncate' href={url}>
+                {name}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
---
+++ components/organisms/navbar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import BottomBar from '@/components/molecules/bottom-bar'
+import Topbar from '@/components/molecules/topbar'
+
+export default function Navbar() {
+  return (
+    <>
+      <Topbar />
+      <BottomBar />
+    </>
+  )
+}
