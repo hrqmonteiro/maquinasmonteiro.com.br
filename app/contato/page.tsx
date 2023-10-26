4f608f82a26b5370bd8fa4bd064df0e543c6decf
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 14:30:32 -0300

    feat: adding contact-page

---
+++ app/contato/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import ContactPage from '@/components/templates/contact-page'
+
+export default function Contato() {
+  return <ContactPage />
+}
---
+++ components/templates/contact-page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Map from '@/components/atoms/map'
+
+export default function ContactPage() {
+  return (
+    <section id='contact' className='min-h-screen'>
+      <Map />
+    </section>
+  )
+}
