184509faabc1348a28cff0d22b316f7ae983968a
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 13:22:07 -0300

    feat: adding footer organism

---
+++ components/organisms/footer.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import BottomFooter from '@/components/molecules/bottom-footer'
+
+import TopFooter from '../molecules/top-footer'
+
+export default function Footer() {
+  return (
+    <footer>
+      <TopFooter />
+      <BottomFooter />
+    </footer>
+  )
+}
