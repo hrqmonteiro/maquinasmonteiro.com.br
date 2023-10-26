9ed3efc8c44528dfdbcecbb27d72b9cde957752b
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 11:15:54 -0300

    feat: adding topbar molecule

---
+++ components/molecules/topbar.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Logo from '@/components/atoms/logo'
+
+export default function Topbar() {
+  return (
+    <div className='my-4 flex items-center justify-between'>
+      <Logo />
+    </div>
+  )
+}
