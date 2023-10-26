36ed3cb88439cfd257988724e4d620a78d7e88f8
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-10-26 13:10:41 -0300

    style: adding colors to support opacity on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,14 @@ export default {
     './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#2D3C96'
-      },
-      textColor: {
-        primary: '#FFFFFF'
-      }
+    colors: {
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+      accent: 'rgb(var(--color-accent) / <alpha-value>)'
+    },
+    textColor: {
+      accent: 'rgb(var(--color-accent) / <alpha-value>)',
+      primary: 'rgb(var(--color-text-primary) / <alpha-value>)'
     }
   },
   plugins: []
